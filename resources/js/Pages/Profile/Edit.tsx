3d5369fb716b06e3043c70d45458993641c87f00
reno <athayapm06@gmail.com>
2026-07-16 19:19:21 +0700

    Fix: double sidebar on profile page and apply sticky header CSS to h1 tags as well

--- resources/js/Pages/Profile/Edit.tsx
+++ resources/js/Pages/Profile/Edit.tsx
@@ -1,4 +1,3 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { PageProps } from '@/types';
 import { Head } from '@inertiajs/react';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm';
@@ -8,22 +7,23 @@ export default function Edit({
     status,
 }: PageProps<{ mustVerifyEmail: boolean; status?: string }>) {
     return (
-        <AuthenticatedLayout
-            header={
-                <h2 className="text-2xl font-bold text-red-800 border-b border-gray-200 pb-4 mb-4">
-                    Ganti Password
-                </h2>
-            }
-        >
+        <div className="p-6 space-y-5">
             <Head title="Ganti Password" />
 
-            <div className="py-12">
-                <div className="mx-auto max-w-7xl space-y-6">
+            <div>
+                <h2 className="text-2xl font-bold text-red-800">
+                    Ganti Password
+                </h2>
+                <p className="text-sm text-red-800 mt-1">Perbarui password akun Anda untuk keamanan.</p>
+            </div>
+
+            <div className="py-6">
+                <div className="mx-auto max-w-7xl">
                     <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
                         <UpdatePasswordForm className="max-w-xl" />
                     </div>
                 </div>
             </div>
-        </AuthenticatedLayout>
+        </div>
     );
 }
